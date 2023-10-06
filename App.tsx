5a92d1fbdb04f2947c9608524d61b06609533a9f
Muhammad Azeem <61928905+Azeemaj101@users.noreply.github.com>
2023-10-06 10:06:44 +0500

    Office

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@
 
 import React, { useState } from 'react';
 import type {PropsWithChildren} from 'react';
-import math from 'mathjs';
+import * as math from 'mathjs';
 import {
   SafeAreaView,
   ScrollView,
@@ -26,28 +26,28 @@ function App(): JSX.Element {
   return (
     <ScrollView>
     <View style={{marginTop:10}}>
-      <TextInput placeholder='0' style={styles.inputCop} value={Display}></TextInput>
+      <TextInput placeholder='0' style={styles.inputCop} editable={false} value={Display}></TextInput>
       <View style={{flex:1, flexDirection: 'row', flexWrap:'wrap'}}>
         
         <Text style={styles.Prebutton} onPress={()=>{SetDisplay(Display+'+')}} >+</Text>
-        <Text style={styles.Prebutton} onPress={()=>{}} >-</Text>
-        <Text style={styles.Prebutton} onPress={()=>{}} >*</Text>
-        <Text style={styles.Prebutton} onPress={()=>{}} >/</Text>
+        <Text style={styles.Prebutton} onPress={()=>{SetDisplay(Display+'-')}} >-</Text>
+        <Text style={styles.Prebutton} onPress={()=>{SetDisplay(Display+'*')}} >*</Text>
+        <Text style={styles.Prebutton} onPress={()=>{SetDisplay(Display+'/')}} >/</Text>
         
         <Text style={styles.button} onPress={()=>{SetDisplay(Display+'7')}} >7</Text>
-        <Text style={styles.button} onPress={()=>{}} >8</Text>
-        <Text style={styles.button} onPress={()=>{}} >9</Text>
+        <Text style={styles.button} onPress={()=>{SetDisplay(Display+'8')}} >8</Text>
+        <Text style={styles.button} onPress={()=>{SetDisplay(Display+'9')}} >9</Text>
 
-        <Text style={styles.button} onPress={()=>{}} >4</Text>
-        <Text style={styles.button} onPress={()=>{}} >5</Text>
-        <Text style={styles.button} onPress={()=>{}} >6</Text>
+        <Text style={styles.button} onPress={()=>{SetDisplay(Display+'4')}} >4</Text>
+        <Text style={styles.button} onPress={()=>{SetDisplay(Display+'5')}} >5</Text>
+        <Text style={styles.button} onPress={()=>{SetDisplay(Display+'6')}} >6</Text>
 
-        <Text style={styles.button} onPress={()=>{}} >1</Text>
-        <Text style={styles.button} onPress={()=>{}} >2</Text>
-        <Text style={styles.button} onPress={()=>{}} >3</Text>
+        <Text style={styles.button} onPress={()=>{SetDisplay(Display+'1')}} >1</Text>
+        <Text style={styles.button} onPress={()=>{SetDisplay(Display+'2')}} >2</Text>
+        <Text style={styles.button} onPress={()=>{SetDisplay(Display+'3')}} >3</Text>
         <Text style={styles.Probutton} onPress={()=>{SetDisplay('')}} >Clear</Text>
         <Text style={styles.Probutton} 
-        onPress={()=>{let a = math.compile("7+7");SetDisplay(String(a));}} >Calc</Text>
+        onPress={()=>{let a = math.evaluate(Display);SetDisplay(String(a));}} >Calc</Text>
       </View>
     </View>
     </ScrollView>
@@ -56,11 +56,13 @@ function App(): JSX.Element {
 
 const styles = StyleSheet.create({
   inputCop: {
-    fontSize:30,
-    borderWidth:2,
+    fontSize:50,
+    fontWeight: '800',
+    borderWidth:5,
     borderColor:"black",
-    borderRadius:23,
+    borderRadius:13,
     marginTop:10,
+    
   },
   Prebutton: {
     marginTop:15,
